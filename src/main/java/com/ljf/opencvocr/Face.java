6ccb29ecf1533693c5cea8a5015057bfe7b827b3
ljf <285079883@qq.com>
2019-04-02 18:06:50 +0800

    init

--- src/main/java/com/ljf/opencvocr/Face.java
+++ src/main/java/com/ljf/opencvocr/Face.java
@@ -56,7 +56,7 @@ public class Face {
 		// 5 把人像区域置为白色
 		// 根据人脸检测得到的矩形位置算出大概人像区域
 		int x1 = (int) (faceRect.x - faceRect.width / 3.5);
-		int y1 = (int) (faceRect.y - faceRect.height / 2);
+		int y1 = (int) (faceRect.y - faceRect.height / 2.0);
 		int w1 = (int) (x1 + faceRect.width * 1.6);
 		int h1 = (int) (y1 + faceRect.height * 2.0);
 		Point point3 = new Point(x1, y1);
